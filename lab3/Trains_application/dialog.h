e477d1ad61eb769042b0396545f13568562194ce
aeshcherbakova <aeshcherbakova@yandex.ru>
2020-11-02 17:11:25 +0300

    Previous version of applied prog deleted

--- lab3/Trains_application/dialog.h
+++ lab3/Trains_application/dialog.h
@@ -36,22 +36,4 @@ namespace Prog3C {
 	int put_in_differrent_cars(train& tr, int num);
 	void clr();
 
-
-    void dialog(train& main_train);
-    void menu();
-    
-    
-    void occupy_seats(train& main_train);
-    void get_carriage_occupancy_information(train& main_train);
-    void add_carriage(train& main_train);
-    void unhook_train(train& main_train);
-    void get_passengers_number(train& main_train);
-
-	void get_average(train& main_train);
-    void all_info(train& main_train);
-	void rearrange_train(train& main_train);
-	void change_places(train& main_train);
-	void add_new_passangers(train& main_train);
-
-	bool comp(struct carriage left, struct carriage right);
 }
